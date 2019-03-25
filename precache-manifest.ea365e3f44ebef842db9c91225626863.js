6afb6db94be7fb0f547b81990e74006fa1ba5305
claramelo <mcmelodecarvalho@gmail.com>
2019-03-24 21:30:11 -0400

    Updates

---
+++ precache-manifest.ea365e3f44ebef842db9c91225626863.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d3fa4bdc3e1341319e07",
+    "url": "/static/js/main.305d7300.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "d3fa4bdc3e1341319e07",
+    "url": "/static/css/main.0c177526.chunk.css"
+  },
+  {
+    "revision": "a6514061baf3acc55a9f7311d7098315",
+    "url": "/index.html"
+  }
+];
